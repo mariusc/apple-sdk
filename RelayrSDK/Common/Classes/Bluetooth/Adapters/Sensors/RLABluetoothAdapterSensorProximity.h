03d15063eaeef0cc6977a4c8b8979387082dd4d2
Jamie Banford <jamie@relayr.de>
2014-08-15 12:02:07 +0200

    Add documentation

--- RelayrSDK/Common/Classes/Bluetooth/Adapters/Sensors/RLABluetoothAdapterSensorProximity.h
+++ RelayrSDK/Common/Classes/Bluetooth/Adapters/Sensors/RLABluetoothAdapterSensorProximity.h
@@ -1,5 +1,10 @@
 #import "RLABluetoothAdapter.h" // Base class
 
+/*!
+ *  @class RLABluetoothAdapterSensorProximity
+ *
+ *  @abstract A Bluetooth adapter class which parses data from a Wunderbar proximity sensor
+ */
 @interface RLABluetoothAdapterSensorProximity : RLABluetoothAdapter
 
 @end
--- RelayrSDK/Common/Classes/Bluetooth/Adapters/Sensors/RLABluetoothAdapterSensorTemperature.h
+++ RelayrSDK/Common/Classes/Bluetooth/Adapters/Sensors/RLABluetoothAdapterSensorTemperature.h
@@ -1,5 +1,10 @@
 #import "RLABluetoothAdapter.h" // Base class
 
+/*!
+ *  @class RLABluetoothAdapterSensorTemperature
+ *
+ *  @abstract A Bluetooth adapter class which parses data from a Wunderbar temperature sensor
+ */
 @interface RLABluetoothAdapterSensorTemperature : RLABluetoothAdapter
 
 @end
